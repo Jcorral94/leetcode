e4af8aae98d70c402b5b76afaf0ae8e667360bf9
Jay <38401811+Jcorral94@users.noreply.github.com>
2026-03-26 19:02:02 -0700

    Update top_travelers.sql

--- top_travelers.sql
+++ top_travelers.sql
@@ -1,9 +1,8 @@
 /* Write your PL/SQL query statement below */
 select
-u.name
-, nvl(sum(r.distance),0) travelled_distance
+distinct u.name
+, nvl(sum(r.distance) over(partition by u.id order by 1),0) travelled_distance
 from users u
 left join rides r on r.user_id = u.id
-group by u.name
 order by 2 desc, 1 asc
 ; 
