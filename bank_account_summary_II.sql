6e8c1ebf166254c4e661d9b1214668587f7287cf
Jay <38401811+Jcorral94@users.noreply.github.com>
2026-03-26 19:58:17 -0700

    Create bank_account_summary_II.sql

---
+++ bank_account_summary_II.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select
+u.name
+, sum(t.amount) balance
+from transactions t
+left join users u on u.account = t.account
+group by u.name
+having sum(t.amount) > 10000;
