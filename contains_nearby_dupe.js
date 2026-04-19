b5b03f4fc96fd7a5ff91f64bae5cdf1ec06c7e1d
Jay <38401811+Jcorral94@users.noreply.github.com>
2026-04-19 16:02:44 -0700

    Create contains_nearby_dupe.js

---
+++ contains_nearby_dupe.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const seen = new Map();
+    for(let i = 0; i < nums.length; i++){
+        if(seen.has(nums[i]) && i - seen.get(nums[i]) <= k ) return true;
+        seen.set(nums[i], i);
+    }
+    return false;
+};
